00afb512d93e0e5d1cc9c9ba00d23e94c08f1a57
Rene061089 <cph-ra147@cphbusiness.dk>
2020-12-11 10:10:05 +0100

    sql Order Db

--- main/java/persistence/mario20201208.sql
+++ main/java/persistence/mario20201208.sql
@@ -1,10 +1,8 @@
-CREATE DATABASE  IF NOT EXISTS `mario` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
-USE `mario`;
--- MySQL dump 10.13  Distrib 8.0.17, for macos10.14 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.22, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: mario
+-- Host: localhost    Database: mario
 -- ------------------------------------------------------
--- Server version	8.0.21
+-- Server version	8.0.22
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -18,6 +16,38 @@ USE `mario`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `order`
+--
+
+DROP TABLE IF EXISTS `order`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `order` (
+                         `order_nr` int NOT NULL,
+                         `pizza_id` int NOT NULL,
+                         `amount` int NOT NULL,
+                         `pickup_time` int NOT NULL,
+                         `order_time` timestamp(2) NOT NULL,
+                         `custemor_name` varchar(45) NOT NULL,
+                         `phone` varchar(45) NOT NULL,
+                         `remove` tinyint NOT NULL,
+                         PRIMARY KEY (`order_nr`),
+                         KEY `pizza_no_idx` (`pizza_id`),
+                         CONSTRAINT `pizzaId` FOREIGN KEY (`pizza_id`) REFERENCES `pizza` (`pizza_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `order`
+--
+
+LOCK TABLES `order` WRITE;
+/*!40000 ALTER TABLE `order` DISABLE KEYS */;
+INSERT INTO `order` VALUES (1,2,6,1260,'2020-12-10 11:06:30.00','rene','51',1),(3,5,5,4444,'2020-12-10 11:30:30.00','kmsdck','65465',1),(4,4,5,1212,'2020-12-10 11:28:30.00','Nicklas','5555',1),(5,5,5,1525,'2020-12-11 08:48:19.00','jaja','888',1);
+/*!40000 ALTER TABLE `order` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `pizza`
 --
 
@@ -25,12 +55,12 @@ DROP TABLE IF EXISTS `pizza`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `pizza` (
-  `pizza_id` int NOT NULL AUTO_INCREMENT,
-  `pizza_no` int NOT NULL,
-  `name` varchar(45) NOT NULL,
-  `ingredients` varchar(45) NOT NULL,
-  `price` int NOT NULL,
-  PRIMARY KEY (`pizza_id`)
+                         `pizza_id` int NOT NULL AUTO_INCREMENT,
+                         `pizza_no` int NOT NULL,
+                         `name` varchar(45) NOT NULL,
+                         `ingredients` varchar(45) NOT NULL,
+                         `price` int NOT NULL,
+                         PRIMARY KEY (`pizza_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -40,7 +70,7 @@ CREATE TABLE `pizza` (
 
 LOCK TABLES `pizza` WRITE;
 /*!40000 ALTER TABLE `pizza` DISABLE KEYS */;
-INSERT INTO `pizza` VALUES (1,1,'Vesuvio','Ost og tomat',57),(2,2,'Hawaii','Ost og ananas',65),(3,3,'Hawaii','Ost og ananas',65),(4,4,'Hawaii','Ost og ananas',65),(5,5,'Rønne dream','Ost og bøf',80),(6,13,'Vegetariana','Grønsager',66);
+INSERT INTO `pizza` VALUES (1,1,'Vesuvio','ost, skinke og tomat',70),(2,2,'Amerikaner','ost, kebab og løg',80),(3,3,'Diavola','Ost, Chillie, pepperonie oghvildløg ',75),(4,4,'Hawai','ost,  skinke og annanas',65),(5,5,'Rønne','ost, tomat, skinke, champingon',88),(6,6,'Halse','Rejer, hvidløg, ost og tomat',90);
 /*!40000 ALTER TABLE `pizza` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -53,4 +83,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-12-08  8:23:26
+-- Dump completed on 2020-12-11 10:04:46
