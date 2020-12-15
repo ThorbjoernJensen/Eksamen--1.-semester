eba942bc097a1697db08df1bc3422bf94e43ef67
Rene061089 <cph-ra147@cphbusiness.dk>
2020-12-15 11:52:00 +0100

    lavet en TODO med

--- main/java/ui/MainMenu.java
+++ main/java/ui/MainMenu.java
@@ -139,7 +139,7 @@ public class MainMenu {
         Pizza chosenPizza = dbMenuCardMapper.getPizzaById(pizzaNo);
         if (chosenPizza == null) {
             System.out.println("Pizza med nr " + pizzaNo + " findes ikke i menuen ");
-            return;
+            return; // TODO Ret så hvis man skriver en pizza der ikke findes skal den fortælle det og efterfølgende give dig lov igen.
         } else {
             int amount = Input.getInt("Indtast antal: ");
             int pickup_time = Input.getInt("Hvad tid ønsker du at hente den: ");
